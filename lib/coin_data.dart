4d79ef50f4ea0258a3240375e8c6e4bae94f8845
selinguz <selin-guzel@hotmail.com>
2023-09-26 00:44:04 +0300

    tüm kripto para birimlerinin seçilen para biriminlerinde karşılığının gelmesi sağlanarak, proje tamamlandı

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -36,26 +36,21 @@ const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
 const apiKey = 'CA9D4481-619F-4B02-8DB0-44CC6DDA7AF9';
 
 class CoinData {
-  //3. Create the Asynchronous method getCoinData() that returns a Future (the price data).
   Future getCoinData(String selectedCurrency) async {
-    //4. Create a url combining the coinAPIURL with the currencies we're interested, BTC to USD.
-    String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
-    //5. Make a GET request to the URL and wait for the response.
-    http.Response response = await http.get(Uri.parse(requestURL));
-
-    //6. Check that the request was successful.
-    if (response.statusCode == 200) {
-      //7. Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
-      var decodedData = jsonDecode(response.body);
-      //8. Get the last price of bitcoin with the key 'last'.
-      var lastPrice = decodedData['rate'];
-      //9. Output the lastPrice from the method.
-      return lastPrice;
-    } else {
-      //10. Handle any errors that occur during the request.
-      print(response.statusCode);
-      //Optional: throw an error if our request fails.
-      throw 'Problem with the get request';
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(Uri.parse(requestURL));
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
+    return cryptoPrices;
   }
 }
